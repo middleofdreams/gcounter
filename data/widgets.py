f3c649e4351890f7cbba4810f04cf7375776a0a3
Kuba <middleofdreams@gmail.com>
2009-11-08 10:05:36 +0100

    dodanie copyrightow i 1 maly bug

--- data/widgets.py
+++ data/widgets.py
@@ -1,3 +1,23 @@
+#
+#       This file is part of GCounter application
+#       
+#       Copyright 2009  Kuba Wrozyna <middleofdreams.wordpress.com>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
 import gtk,gtk.glade
 
 def assignwidgets(mainclass):
@@ -47,7 +67,8 @@ def assignwidgets(mainclass):
 		"prefscancel":	mainclass.prefscancel}
 		mainclass.wTree.signal_autoconnect(dic)
 
-def createstatusicon(mainclass,dir):		
+def createstatusicon(mainclass,dir):	
+	
 		mainclass.window.set_icon_from_file(dir+"/icon.png") 
 		mainclass.staticon = gtk.StatusIcon() 
 		mainclass.staticon.set_from_file(dir+"/icon.png") 
--- gcounter.py
+++ gcounter.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#       This file is part of GCounter application
+#       
+#       Copyright 2009  Kuba Wrozyna <middleofdreams.wordpress.com>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
 
 #loading main libraries
 import sys,pygtk,gtk,gtk.glade,time,os,pango
@@ -63,7 +82,7 @@ class gcounter:
 	#handlers for gui actions:
 		
 	def prefscancel(self,widget):
-		preferences.loadmainprefs(self)
+		self.gcprefs.loadmainprefs(self)
 		self.prefwindow.hide()
 
 	def prefssave(self,widget):
